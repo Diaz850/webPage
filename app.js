095335762b52fadeeacd9e5a097b8fa8ccf619a7
Pedro Diaz Jr. <Diaz850@gmail.com>
2023-10-15 09:41:49 -0400

    reworded the prompt command

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('Welcome to the console page!')
 
-let userAge = prompt('How old are you?')
+let userAge = prompt('MUST BE 21 OR OVER PLEASE ENTER AGE')
 if (userAge >= 21) {
     document.write('Welcome to the page!');
 }
